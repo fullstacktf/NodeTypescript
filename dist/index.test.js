1921890e12d0473c2d6481082c5d4f9cabbe596e
Kevin Eliezer <kevineli.gp@gmail.com>
2020-11-12 18:34:56 +0000

    Añadiendo gitignore

--- dist/index.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("./utils");
-test('should return the hello response', () => {
-    expect(utils_1.getHelloMessage()).toBe('Hello');
-});
